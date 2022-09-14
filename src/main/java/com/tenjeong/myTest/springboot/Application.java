ce118d417de6121b7f83660ffeb0f29fba44ed7c
TenJeong <tenj845@gmail.com>
2022-09-14 23:07:08 +0900

    레퍼지토리 생성

---
+++ src/main/java/com/tenjeong/myTest/springboot/Application.java
@@ -0,0 +1,12 @@
+package com.tenjeong.myTest.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
