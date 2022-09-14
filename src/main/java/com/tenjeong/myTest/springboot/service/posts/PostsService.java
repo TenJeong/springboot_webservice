ae972463c21063796b68c581a59a2f2d3a99af0e
TenJeong <tenj845@gmail.com>
2022-09-15 03:18:35 +0900

    글 조회 목록 표시 추가

--- src/main/java/com/tenjeong/myTest/springboot/service/posts/PostsService.java
+++ src/main/java/com/tenjeong/myTest/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.tenjeong.myTest.springboot.service.posts;
 
 import com.tenjeong.myTest.springboot.domain.posts.Posts;
 import com.tenjeong.myTest.springboot.domain.posts.PostsRepository;
+import com.tenjeong.myTest.springboot.web.dto.PostsListResponseDto;
 import com.tenjeong.myTest.springboot.web.dto.PostsResponseDto;
 import com.tenjeong.myTest.springboot.web.dto.PostsSaveRequestDto;
 import com.tenjeong.myTest.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +38,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
