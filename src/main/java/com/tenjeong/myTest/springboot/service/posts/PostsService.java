2b0ed9da6d4e9bf4221328c02ecd09025bc3310d
TenJeong <tenj845@gmail.com>
2022-09-15 01:20:31 +0900

    posts 등록 Api 추가

---
+++ src/main/java/com/tenjeong/myTest/springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.tenjeong.myTest.springboot.service.posts;
+
+import com.tenjeong.myTest.springboot.domain.posts.PostsRepository;
+import com.tenjeong.myTest.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
